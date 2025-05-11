9aedf3afb1a2829d187834d894ac08a584c65f70
Stefanurkal <stephenfranada@gmail.com>
2025-05-11 14:26:33 -0700

    completed

--- usestate/src/App.jsx
+++ usestate/src/App.jsx
@@ -1,11 +1,15 @@
 import RestaurantsContainer from "./components/RestaurantsContainer";
 import { restaurants } from "./data/data.js";
 import "./App.css";
+import { useState } from "react"; 
 
 function App() {
+  
+  const [restaurantState, setRestaurants] = useState([...restaurants]);
+  
   return (
     <div className="App">
-      <RestaurantsContainer />
+      <RestaurantsContainer restaurants={restaurantState} />
     </div>
   );
 }
